9a536ef5cf47f43b0447faafb046962c9452c335
varsh <varsh@DESKTOP-I76O0QA>
2020-05-28 07:49:54 -0700

    updated remove and get methods.

--- DataStructureLibrary/src/in/ac/sdmcet/ds/linkedList/SingleLinkedList.java
+++ DataStructureLibrary/src/in/ac/sdmcet/ds/linkedList/SingleLinkedList.java
@@ -5,30 +5,60 @@ package in.ac.sdmcet.ds.linkedList;
  *
  * @param <T>
  */
-public class SingleLinkedList<T> implements LinkedList<T> {
+class SingleLinkedList<T> implements LinkedList<T> {
 	private SingleLinkedNode<T> head = null;
-    
+	
 	@Override
 	public void add(T data) {
 		if (head == null) {
 			head = new SingleLinkedNode<T>(data);
-		} else {
+		}
+		else {
 			SingleLinkedNode<T> node = head;
 			while (node.getNext() != null) {
 				node = node.getNext();
 			}
 			node.setNext(new SingleLinkedNode<T>(data));
 		}
+	}   
+	public SingleLinkedNode<T> getHead(){
+		return head;
 	}
-
+	
+	//deleting the element based on location specified
 	@Override
 	public T remove(int i) {
+		int myItr;
+		SingleLinkedNode<T> temp = null;
+		SingleLinkedNode<T> trav = null;
+		SingleLinkedNode<T> dump = null;
+		if(i == 1) {
+			head = null;
+			return null;
+		}
+		temp = head.getNext();
+		trav = head;
+		for(myItr=1; myItr < i-1; myItr++) {
+			temp = temp.getNext();
+			trav = trav.getNext();
+		}
+		dump = trav.getNext();
+		temp = temp.getNext();
+		trav.setNext(dump.getNext());
+		dump.setNext(null);
 		return null;
 	}
-
+	
+	//get the element based on location specified
 	@Override
 	public T get(int i) {
-		return null;
-	}
+		SingleLinkedNode<T> trav = null;
+		trav = head;
+		int myItr = 0;
+		for(myItr=0; myItr < i; myItr++) {
+			trav = trav.getNext();
+		}
+		return trav.getData();
+    }
 
 }
