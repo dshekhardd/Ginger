0861dc9d2d3e73f55c79ddc3aebc6aed25431979
varsh <varsh@DESKTOP-I76O0QA>
2020-05-28 07:54:24 -0700

    First Check-in

--- DataStructureLibrary/src/in/ac/sdmcet/ds/linkedList/LinkedList.java
+++ DataStructureLibrary/src/in/ac/sdmcet/ds/linkedList/LinkedList.java
@@ -10,7 +10,7 @@ package in.ac.sdmcet.ds.linkedList;
 public interface LinkedList<T> {
 	public void add(T data); 
 
-	public T remove(int i);
+	public T remove(int i); //based on location
 
 	public T get(int i);
 }
